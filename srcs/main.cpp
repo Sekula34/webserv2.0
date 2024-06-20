bfaccaf319e44d9a663da415b1d71ffd742eee70
Filip Seles <fseles@c3r8p1.42vienna.com>
2024-06-20 19:10:41 +0200

    just: some main to test

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,9 +1,24 @@
 #include "./Parsing/ServersInfo.hpp"
+#include "Parsing/ServerSettings.hpp"
+#include <exception>
+#include <iostream>
 
 int main()
 {
-	ServersInfo servers;
 
-	servers.printAllServersInfo();
+	try
+	{
+		ServersInfo servers;
+
+		
+		std::vector<ServerSettings> serveri = servers.getAllServers();
+		servers.printAllServersInfo();
+
+	}
+	catch(std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
 	return 0;
 }
