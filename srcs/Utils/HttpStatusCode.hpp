da7a96a2914863ce9fb4f65e2cb0d4deba80a1c0
Filip Seles <fseles@c2r5p5.42vienna.com>
2024-07-05 14:06:14 +0200

    just: added some comments

--- srcs/Utils/HttpStatusCode.hpp
+++ srcs/Utils/HttpStatusCode.hpp
@@ -4,6 +4,11 @@
 #include <map>
 #include <string>
 
+/**
+ * @brief static class for getting ReasonPhrase.
+ * example 404 will return Not Found. 200 will return OK
+ * 
+ */
 class HttpStatusCode 
 {
 	private : 
@@ -11,6 +16,13 @@ class HttpStatusCode
 		static std::map<int, std::string> _statusCode;
 
 	public:
+		/**
+		 * @brief Get the Reason Phrase message behind code
+		 * 
+		 * @param code code that you want to turn in Reason
+		 * @throw UnkownCodeException if cannot find code in static map _status code
+		 * @return std::string 
+		 */
 		static std::string getReasonPhrase(int code);
 
 		class UnkownCodeException : public std::exception
