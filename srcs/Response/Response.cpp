e28216e3ac78140002834e5115c4b2e96022594e
Filip Seles <fseles@c2r5p5.42vienna.com>
2024-07-06 13:37:03 +0200

    feat: ResponseClass will have ClientHeader and ClientBody

--- srcs/Response/Response.cpp
+++ srcs/Response/Response.cpp
@@ -1,14 +1,15 @@
 #include "Response.hpp"
+#include "ResponseHeader.hpp"
 #include <ostream>
 
-Response::Response(int& clientFd)
-:_clientFd(clientFd), _code(-1), _body("")
+Response::Response(int& clientFd, const ResponseHeader& header)
+:_clientFd(clientFd),_header(header), _code(-1), _body("")
 {
 
 }
 
 Response::Response(const Response& source)
-:_clientFd(source._clientFd), _body(source._body)
+:_clientFd(source._clientFd), _header(source._header), _body(source._body)
 {
 	
 }
@@ -28,6 +29,8 @@ std::ostream& operator<<(std::ostream& os, const Response& obj)
 {
 	os << "Response for client: " << obj._clientFd << std::endl;
 	os << "Code of response is: " << obj._code << std::endl;
+	os << "Response header is :" << std::endl;
+	os << obj._header.turnResponseHeaderToString();
 	os << "Body of response is : " << obj._body << std::endl;
 	return os;
 }
--- srcs/Response/Response.hpp
+++ srcs/Response/Response.hpp
@@ -2,18 +2,20 @@
 # define RESPONSE_HPP
 #include <ostream>
 #include <string>
+#include "ResponseHeader.hpp"
 
 class Response 
 {
 	private :
 		int& _clientFd;
+		const ResponseHeader& _header;
 		int _code;
 		//CLASS HEADER;
 		std::string _body; 
 		//Response();
 
 	public :
-		Response(int& _clientFd);
+		Response(int& _clientFd, const ResponseHeader& header);
 		Response(const Response& source);
 		Response& operator=(const Response& source);
 		~Response();
