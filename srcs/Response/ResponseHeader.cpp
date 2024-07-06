63b1e3c0917fdeb22d8c316c7d3d4d05befb8bfc
Filip Seles <fseles@c2r5p5.42vienna.com>
2024-07-06 13:05:38 +0200

    feat:

---
+++ srcs/Response/ResponseHeader.cpp
@@ -0,0 +1,84 @@
+#include "ResponseHeader.hpp"
+#include "../Utils/HttpStatusCode.hpp"
+#include "../Parsing/ParsingUtils.hpp"
+
+ResponseHeader::ResponseHeader(int& httpCode)
+:_httpCode(httpCode)
+{
+	_fillStatusLineElements();
+	_headerFields["Connection"] = "close";
+}
+
+ResponseHeader::ResponseHeader(const ResponseHeader& source)
+:_httpCode(source._httpCode), _statusLine(source._statusLine), _headerFields(source._headerFields)
+{
+
+}
+
+ResponseHeader& ResponseHeader::operator=(const ResponseHeader& source)
+{
+	(void) source;
+	return (*this);
+}
+
+ResponseHeader::~ResponseHeader()
+{
+
+}
+
+
+
+
+void ResponseHeader::_fillStatusLineElements()
+{
+	_statusLine.HttpVersion = "HTTP/1.1";
+	_statusLine.statusCode = _httpCode;
+	_statusLine.ReasonPhrase =  HttpStatusCode::getReasonPhrase(_httpCode);
+}
+
+//"HTTP/1.1 200 OK\r\n"
+std::string ResponseHeader::_getStatusLineAsString() const
+{
+	std::string statusLine;
+	statusLine += _statusLine.HttpVersion;
+	statusLine += " ";
+	statusLine += ParsingUtils::intToString(_statusLine.statusCode);
+	statusLine += " ";
+	statusLine += _statusLine.ReasonPhrase;
+	statusLine += "\r\n";
+	return statusLine;
+}
+
+std::string ResponseHeader::_getOneHeaderFieldAsString(std::string key, std::string value) const 
+{
+	std::string oneHeaderField;
+	oneHeaderField += key;
+	oneHeaderField += ": ";
+	oneHeaderField += value;
+	oneHeaderField += "\r\n";
+	return oneHeaderField;
+}
+
+// "Content-Type: text/html\r\n"
+// "Content-Length: 48\r\n"
+// "Connection: close\r\n"
+std::string ResponseHeader::_getAllHeaderFieldsAsString() const
+{
+	std::string headerFields;
+	std::map<std::string, std::string>::const_iterator it;
+	for(it = _headerFields.begin(); it != _headerFields.end(); it++)
+	{
+		headerFields += _getOneHeaderFieldAsString(it->first, it->second);
+	}
+	return headerFields;
+}
+
+
+
+std::string ResponseHeader::turnResponseHeaderToString(void) const
+{
+	std::string fullHeader;
+	fullHeader += _getStatusLineAsString();
+	fullHeader += _getAllHeaderFieldsAsString();
+	return fullHeader;
+}
---
+++ srcs/Response/ResponseHeader.hpp
@@ -0,0 +1,39 @@
+#ifndef RESPONSEHEADER_HPP
+# define RESPONSEHEADER_HPP
+#include <string>
+#include <map>
+
+struct StatusLineElements
+{
+	std::string HttpVersion;
+	int statusCode;
+	std::string ReasonPhrase;
+};
+
+class ResponseHeader
+{
+	private:
+		int& _httpCode;
+		StatusLineElements _statusLine;
+		std::map<std::string, std::string> _headerFields;
+
+		void _fillStatusLineElements();
+		std::string _getStatusLineAsString() const;
+		std::string _getOneHeaderFieldAsString(std::string key, std::string value) const;
+		std::string _getAllHeaderFieldsAsString() const;
+
+	public:
+		ResponseHeader(int& httpCode);
+		ResponseHeader(const ResponseHeader& source);
+		ResponseHeader& operator=(const ResponseHeader& source);
+		~ResponseHeader();
+
+		std::string turnResponseHeaderToString(void) const;
+
+		/*void setContentLength(int contentLength);
+		void setDate(void);
+		void setContentLanguage(std::string language = "en");
+		*/
+};
+
+#endif
