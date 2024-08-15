3bbcfefdd9d3d2c9b3d111c598b3e2578e60d917
Sekula34 <filip.seles@gmail.com>
2024-08-15 19:04:13 +0200

    feat: implementing new class UrlSuffix

---
+++ srcs/Utils/UrlSuffix.cpp
@@ -0,0 +1,50 @@
+#include "UrlSuffix.hpp"
+#include "Logger.hpp"
+
+
+UrlSuffix::UrlSuffix(const std::string urlSuffix)
+:_urlSuffix(urlSuffix)
+{
+	_initVars();
+	_parseAndSetPath();
+}
+
+UrlSuffix::UrlSuffix(const UrlSuffix& source) 
+:_urlSuffix(source._urlSuffix)
+{
+	//parse stuff from suffix	
+}
+
+UrlSuffix& UrlSuffix::operator=(const UrlSuffix source)
+{
+	Logger::warning("Give me good reason why you called this on URL suffix");
+	(void) source;
+	return (*this);
+}
+
+UrlSuffix::~UrlSuffix()
+{
+
+}
+
+const std::string& UrlSuffix::getPath() const
+{
+	return (_path);
+}
+
+
+void UrlSuffix::_initVars()
+{
+	_path = "";
+	_queryParameters = "";
+	_fragment = "";
+}
+
+void UrlSuffix::_parseAndSetPath()
+{
+	size_t questionMarkPos = _urlSuffix.find('?');
+	if(questionMarkPos == std::string::npos)
+		_path = _urlSuffix;
+	_path = _urlSuffix.substr(0, questionMarkPos);
+	
+}
---
+++ srcs/Utils/UrlSuffix.hpp
@@ -0,0 +1,29 @@
+#ifndef URLSUFFIX_HPP
+# define URLSUFFIX_HPP
+#include <string>
+
+class UrlSuffix 
+{
+	public:
+		UrlSuffix(const std::string urlSuffix);
+		UrlSuffix(const UrlSuffix& source);
+		UrlSuffix& operator=(const UrlSuffix source);
+		~UrlSuffix();
+
+		const std::string& getPath() const;
+
+
+	private:
+		std::string _path; //can only be ?
+		std::string _queryParameters;
+		std::string _fragment;
+
+		const std::string _urlSuffix;
+
+		void _initVars();
+		void _parseAndSetPath();
+		void _parseAndSetQuerryParameters();
+		void _parseAndSetFragmetn();
+};
+
+#endif
