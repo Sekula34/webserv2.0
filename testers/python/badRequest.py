cd6337b3f462030258406d05a4ea75a8884e5bae
Sekula34 <filip.seles@gmail.com>
2024-08-17 14:21:11 +0200

    adding some edge cases to python tester

--- testers/python/badRequest.py
+++ testers/python/badRequest.py
@@ -1,10 +1,37 @@
 import socket
 from colors import Colors
+import time 
 
-host ="localhost"
-port = 9090
+HOST ="localhost"
+PORT = 9090
+
+class RequestData():
+	def __init__(self, title, curl_request, expected_http_status):
+		self.__title = title
+		self.__curl_request = curl_request
+		self.__expecetd_http_status = expected_http_status
+
+	def print(self):
+		mesage = "Data info:\n\ttitle:[{0}]\n\
+				\r\trequest:[{1}]".format(self.__title, self.__curl_request)
+		text = Colors.color_text(mesage, Colors.OKCYAN)
+		print(text)
+
+	def send(self):
+		text = Colors.color_text("Sending custom request", Colors.WARNING)
+		print(text)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect((HOST, PORT))
+		sock.sendall(self.__curl_request.encode("utf-8"))
+		server_response = sock.recv(4096)
+		print(server_response.decode("utf-8"))
+		sock.close()
+		
+
 
 def badRequest():
+	host = "localhost"
+	port = 9090
 	text = Colors.color_text("Sending Bad Request", Colors.FAIL)
 	print(text)
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,8 +42,26 @@ def badRequest():
 	print(server_response.decode("utf-8"))
 	sock.close()
 
+def no_host_request():
+	text = Colors.color_text("Sending requsest with no host", Colors.FAIL)
+	print(text)
+
+
+def long_timeout():
+	req = RequestData("Long Timeout", "get", 400)
+	start = time.time()
+	req.send()
+	end = time.time()
+	print("Response took: {0} seconds".format(end - start))
+
+def seg_fault():
+	curl_request = "GET / HTTP/1.1\r\nHost: example.com\r\nUser-Agent: CustomClient\r\n\r\n"
+	req = RequestData("Confusing Host", curl_request, 400)
+	req.send()
+
 def main():
-	badRequest()
+	seg_fault()
+	long_timeout()
 
 if __name__ == "__main__":
 	main()
