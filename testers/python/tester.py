038f0e22fb7a44b97a1be33cd57661d1a8c509f9
Sekula34 <filip.seles@gmail.com>
2024-09-04 12:24:03 +0200

    feat: adding resposne time counter for every get request, added test case for cgi io script, some minor output text changes

--- testers/python/tester.py
+++ testers/python/tester.py
@@ -15,16 +15,25 @@ class TestMyWebServer(unittest.TestCase):
 		title = Colors.test_case_title("\nTesting:" + text)
 		print(title)
 
+	@staticmethod
+	def send_get(url):
+		start = time.time()
+		response = requests.get(url)
+		end = time.time()
+		miliseconds = round((end - start) * 1000, 3)
+		print("Response took: {0} milliseconds(ms)".format(miliseconds))
+		return response
+
 	def test_home_page(self):
 		TestMyWebServer.print_test_title("Home page")
-		response = requests.get("http://localhost:9090/")
+		response = TestMyWebServer.send_get("http://localhost:9090/")
 		self.assertEqual(response.status_code, 200)
 		self.assertIn("Welcome to Server 9090", response.text)
 		Colors.test_passed()
 	
 	def test_404_page(self):
 		TestMyWebServer.print_test_title("Error 404")
-		response = requests.get("http://localhost:9090/idondtexist/")
+		response = TestMyWebServer.send_get("http://localhost:9090/idondtexist/")
 		self.assertEqual(response.status_code, 404)
 		Colors.test_passed()
 	
@@ -36,38 +45,38 @@ class TestMyWebServer(unittest.TestCase):
 	
 	def test_other_file(self):
 		TestMyWebServer.print_test_title("Files that are not part of location")
-		respones = requests.get("http://localhost:8080/hej/Socket.cpp")
+		respones = TestMyWebServer.send_get("http://localhost:8080/hej/Socket.cpp")
 		self.assertEqual(respones.status_code, 200)
 		self.assertIn("Socket", respones.text)
-		respones = requests.get("http://localhost:8080/hej/Socket.hpp")
+		respones = TestMyWebServer.send_get("http://localhost:8080/hej/Socket.hpp")
 		self.assertEqual(respones.status_code, 200)
 		self.assertIn("Socket", respones.text)
 		Colors.test_passed()
 
 	def test_autoindex(self):
 		TestMyWebServer.print_test_title("Testing autoindex")
-		response = requests.get("http://localhost:8080/autoindex/")
+		response = TestMyWebServer.send_get("http://localhost:8080/autoindex/")
 		self.assertEqual(response.status_code, 200)
 		self.assertIn("Auto index of folder:", response.text)
 		Colors.test_passed()
 
 	def test_autoindexSubfolder(self):
 		TestMyWebServer.print_test_title("Testing autoindex subfolder")
-		response = requests.get("http://localhost:8080/autoindex/subfolder/")
+		response = TestMyWebServer.send_get("http://localhost:8080/autoindex/subfolder/")
 		self.assertEqual(response.status_code, 200)
 		self.assertIn("Auto index of folder:", response.text)
 		Colors.test_passed()
 
 	def test_autoindexThatHaveIndex(self):
 		TestMyWebServer.print_test_title("Testing autoindex that have landing page")
-		response = requests.get("http://localhost:8080/hej/")
+		response = TestMyWebServer.send_get("http://localhost:8080/hej/")
 		self.assertEqual(response.status_code, 200)
 		self.assertIn("Socket.hpp", response.text)
 		Colors.test_passed()
 
 	def test_autoindexBlocked(self):
 		TestMyWebServer.print_test_title("Testing autoindex that is blocked")
-		response = requests.get("http://localhost:8080/autoindexBlocked/")
+		response = TestMyWebServer.send_get("http://localhost:8080/autoindexBlocked/")
 		self.assertEqual(response.status_code, 404)
 		self.assertIn("404", response.text)
 		Colors.test_passed()
@@ -98,28 +107,32 @@ class TestMyWebServer(unittest.TestCase):
 
 	def test_cgi_io_test(self):
 		TestMyWebServer.print_test_title("Testing cgi io test")
-		start = time.time()
-		response = requests.get("http://localhost:9090/cgi-bin/io_test.py")
-		end = time.time()
-		print("Response took: {0} seconds".format(end - start))
+		response = TestMyWebServer.send_get("http://localhost:9090/cgi-bin/io_test.py")
 		self.assertEqual(response.status_code, 200)
 		self.assertIn("Environment Variables", response.text)
 		Colors.test_passed()
 
-	def spammer(self, numberOfRequest = 500):
+	def spammer(self, url, numberOfRequest = 50):
 		accepted_count = 0
-		TestMyWebServer.print_test_title("Spammer on " + str(numberOfRequest))
+		total_accept_time = 0
+		TestMyWebServer.print_test_title("Spammer on " + str(numberOfRequest) + ": " + url)
 		for i in range (0, numberOfRequest):
 			try:
-				response = requests.get("http://localhost:8080/hej/Socket.hpp", timeout=0.1)
+				start = time.time()
+				response = requests.get(url, timeout=0.1)
 				if response.status_code == 200:
+					end = time.time()
 					accepted_count += 1
+					total_accept_time += end - start
 			except requests.exceptions.RequestException as e:
 				print(f"request {i} failed: {e}")
-		self.__spammerResult(numberOfRequest, accepted_count)
+		self.__spammerResult(numberOfRequest, accepted_count, total_accept_time)
 
-	def __spammerResult(self, numberOfRequst, accpted):
+	def __spammerResult(self, numberOfRequst, accpted, total_accept_time):
 		result = "Spammer done {0}/{1} test accepted".format(accpted, numberOfRequst)
+		if(accpted != 0):
+			avr_response_time_ms = round((total_accept_time/ accpted) * 1000, 3)
+			result += ". Average time to get response in miliscedons is {0}".format(avr_response_time_ms)
 		if(accpted == numberOfRequst):
 			result = Colors.color_text(result, Colors.OKGREEN)
 		else:
