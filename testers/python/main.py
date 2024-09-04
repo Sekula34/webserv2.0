12ad0194f11374b089533f08b8ab6b32d3632637
Sekula34 <filip.seles@gmail.com>
2024-09-04 12:26:26 +0200

    feat: added spamming on both cgi and non cgi, changing default value of 50 spam request instead of 500 beacuse valgrind make it really slow

--- testers/python/main.py
+++ testers/python/main.py
@@ -14,17 +14,22 @@ def spammer(testInstance):
 	question = Colors.color_text("Do you want to run spammer?\n\t1 - Yes\n\t0 - NO\nAnswer: ", Colors.BOLD)
 	user_answer = input(question)
 	if(user_answer == "1"):
-		spammerInput(testInstance)
+		output_text = Colors.color_text("Spamming on cgi", Colors.OKBLUE)
+		print(output_text)
+		spammerInput(testInstance, "http://localhost:9090/cgi-bin/io_test.py")
+		output_text = Colors.color_text("\nSpamming on non cgi", Colors.OKBLUE)
+		print(output_text)
+		spammerInput(testInstance, "http://localhost:9090/")
 	else:
 		print("Spammer test is skipped")
 		
 
-def spammerInput(testInstance):
-	question = Colors.color_text("How many request you want to send?[leave empty for default case of 500]: ", Colors.BOLD)
+def spammerInput(testInstance, url):
+	question = Colors.color_text("How many request you want to send?[leave empty for default case of 50]: ", Colors.BOLD)
 	answer = input(question)
 	if(answer == ""):
-		answer = 500
-	testInstance.spammer(int(answer))
+		answer = 50
+	testInstance.spammer(url, int(answer))
 
 
 def big_main():
