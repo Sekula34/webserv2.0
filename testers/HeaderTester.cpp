112295f8890bd3dea31e1918bed5995f5ecbe2a3
Sekula34 <filip.seles@gmail.com>
2024-09-09 15:06:28 +0200

    tester: preparing testCases for testing new Header hierarchy

---
+++ testers/HeaderTester.cpp
@@ -0,0 +1,9 @@
+#include "HeaderTester.hpp"
+#include "../srcs/Client/AHeader.hpp"
+
+
+void HeaderTester::testAll()
+{
+	std::string headerSection("");
+	//AHeader("")
+}
---
+++ testers/HeaderTester.hpp
@@ -0,0 +1,12 @@
+#pragma once 
+#include "UnitTest.hpp"
+
+
+
+class HeaderTester : UnitTest
+{
+	public:
+		static void testAll();
+
+	private:
+};
