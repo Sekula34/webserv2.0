a4c260b5a77de0ef64eb2df10293896fecae670d
Sekula34 <filip.seles@gmail.com>
2024-09-23 18:46:12 +0200

    refactor: maronetting Token class

--- srcs_new/Parsing/Token.cpp
+++ srcs_new/Parsing/Token.cpp
@@ -4,13 +4,122 @@
 #include <vector>
 
 
+//Create Token out of info and set default value of type to be UNKOWN, and lineNumber to 0
+//check everyContext in AllTokens
+//throw InvalidTokenException if there is Unkown context
+//set _contextType while checking
+void Token::checkAllTokensContext(std::vector<Token>& allTokens)
+{
+	for(size_t i = 0; i < allTokens.size(); i++)
+	{
+		Token& oneToken(allTokens[i]);
+		oneToken._checkOneToken();
+	}
+}
 
+//print all infromation of every token in allTokens
+void Token::printAllTokensInfo(const std::vector<Token>& allTokens)
+{
+	for(size_t i = 0; i < allTokens.size(); i++)
+	{
+		allTokens[i].printTokenInfo();
+	}
+}
+
+//return vector of Tokens that are part of server matching serverId, not including braces
+std::vector<Token> Token::getAllServerTokens(int serverId, std::vector<Token>& allTokens)
+{
+	std::vector<Token> serverTokens;
+	//Token::printAllTokensInfo(allTokens);
+	for(size_t i = 0; i < allTokens.size(); i++)
+	{
+		std::vector<Token> tokenPath = allTokens[i].getTokenPath();
+		if(tokenPath.size() < 2)
+			continue;
+		if(tokenPath[1].getTokenContextType() == SERVER && tokenPath[1].getTokenServerId() == serverId)
+		{
+			if(allTokens[i].getTokenType() == OPENING_BRACE || allTokens[i].getTokenType() == CLOSING_BRACE)
+				continue;
+			serverTokens.push_back(allTokens[i]);
+		}
+	}
+	return serverTokens;
+}
+
+Token::TokenType Token::getTokenType(void) const
+{
+	return (_type);
+}
+
+Token::ContextType Token::getTokenContextType(void) const 
+{
+	return(_contextType);
+}
+
+int Token::getTokenServerId(void) const
+{
+	return (_serverId);
+}
+
+size_t Token::getTokenLineNumber() const
+{
+	return (_lineNumber);
+}
+
+std::string Token::getTokenInfo() const
+{
+	return (_info);
+}
+
+Token::ContextType Token::getCurrentTokenContextType() const
+{
+	return _contextType;
+}
+
+std::vector<Token> Token::getTokenPath(void) const
+{
+	return (_path);
+}
+
+void Token::setTokenPath(std::vector<Token> path)
+{
+	_path = path;
+}
+
+//set token type of enum TokenTYpe
+void Token::setTokenType(TokenType newType)
+{
+	_type = newType;
+}
+
+void Token::printTokenInfo(void) const
+{
+	std::cout << "---------------TOKEN INFO---------------"<<std::endl;
+	std::cout << "Token content: " << _info << std::endl;
+	std::cout << "Server id :" << _serverId << std::endl;
+	std::cout << "Token type: "<<_type << std::endl;
+	std::cout << "Context type is " << _contextType << std::endl;
+	std::cout << "Token line number: " << _lineNumber << std::endl;
+	std::cout << "Token path is :";
+	for(size_t i = 0; i < _path.size(); i++)
+	{
+		std::cout << _path[i]._info;
+		if(i != _path.size() - 1)
+			std::cout <<" ";
+	}
+	std::cout <<std::endl;
+	std::cout << "Token context type is " << _contextType << std::endl;
+	std::cout << "________________________________________" << std::endl;
+}
 
 Token::Token()
 {
-
+	_info = "";
+	_type = UNKOWN;
+	_lineNumber = 0;
+	_serverId = 0;
+	_contextType = NOT_CONTEXT;
 }
-//Create Token out of info and set default value of type to be UNKOWN, and lineNumber to 0
 // TYPE will be then auto seted if info is opening or closing bracket 
 // if string ends with ; it is directive, otherwise it is Context
 // use setTokenType function to give type to be context or Directive
@@ -98,114 +207,6 @@ void Token::_contextChecker(void)
 	throw InvalidTokenException();
 }
 
-//check everyContext in AllTokens
-//throw InvalidTokenException if there is Unkown context
-//set _contextType while checking
-void Token::checkAllTokensContext(std::vector<Token>& allTokens)
-{
-	for(size_t i = 0; i < allTokens.size(); i++)
-	{
-		Token& oneToken(allTokens[i]);
-		oneToken._checkOneToken();
-		//std::cout <<"Checking printer" << std::endl;
-		//oneToken.printTokenInfo();
-	}
-}
-
-//print all infromation of every token in allTokens
-void Token::printAllTokensInfo(const std::vector<Token>& allTokens)
-{
-	for(size_t i = 0; i < allTokens.size(); i++)
-	{
-		allTokens[i].printTokenInfo();
-	}
-}
-
-//return vector of Tokens that are part of server matching serverId, not including braces
-std::vector<Token> Token::getAllServerTokens(int serverId, std::vector<Token>& allTokens)
-{
-	std::vector<Token> serverTokens;
-	//Token::printAllTokensInfo(allTokens);
-	for(size_t i = 0; i < allTokens.size(); i++)
-	{
-		std::vector<Token> tokenPath = allTokens[i].getTokenPath();
-		if(tokenPath.size() < 2)
-			continue;
-		if(tokenPath[1].getTokenContextType() == SERVER && tokenPath[1].getTokenServerId() == serverId)
-		{
-			if(allTokens[i].getTokenType() == OPENING_BRACE || allTokens[i].getTokenType() == CLOSING_BRACE)
-				continue;
-			serverTokens.push_back(allTokens[i]);
-		}
-	}
-	return serverTokens;
-}
-
-TokenType Token::getTokenType(void) const
-{
-	return (_type);
-}
-
-ContextType Token::getTokenContextType(void) const 
-{
-	return(_contextType);
-}
-
-int Token::getTokenServerId(void) const
-{
-	return (_serverId);
-}
-
-size_t Token::getTokenLineNumber() const
-{
-	return (_lineNumber);
-}
-
-std::string Token::getTokenInfo() const
-{
-	return (_info);
-}
-
-ContextType Token::getCurrentTokenContextType() const
-{
-	return _contextType;
-}
-
-std::vector<Token> Token::getTokenPath(void) const
-{
-	return (_path);
-}
-
-void Token::setTokenPath(std::vector<Token> path)
-{
-	_path = path;
-}
-
-//set token type of enum TokenTYpe
-void Token::setTokenType(TokenType newType)
-{
-	_type = newType;
-}
-
-void Token::printTokenInfo(void) const
-{
-	std::cout << "---------------TOKEN INFO---------------"<<std::endl;
-	std::cout << "Token content: " << _info << std::endl;
-	std::cout << "Server id :" << _serverId << std::endl;
-	std::cout << "Token type: "<<_type << std::endl;
-	std::cout << "Context type is " << _contextType << std::endl;
-	std::cout << "Token line number: " << _lineNumber << std::endl;
-	std::cout << "Token path is :";
-	for(size_t i = 0; i < _path.size(); i++)
-	{
-		std::cout << _path[i]._info;
-		if(i != _path.size() - 1)
-			std::cout <<" ";
-	}
-	std::cout <<std::endl;
-	std::cout << "Token context type is " << _contextType << std::endl;
-	std::cout << "________________________________________" << std::endl;
-}
 
 
 const char * Token::InvalidTokenException::what() const throw()
--- srcs_new/Parsing/Token.hpp
+++ srcs_new/Parsing/Token.hpp
@@ -12,58 +12,58 @@ const std::string resetText = "\033[0m";
 const std::string validContexts[] = {"http", "server", "location"};
 
 
-enum TokenType{
-	UNKOWN,
-	OPENING_BRACE,
-	CLOSING_BRACE,
-	CONTEXT,
-	DIRECTIVE
-};
-
-enum ContextType
-{
-	HTTP,
-	SERVER,
-	LOCATION,
-	NOT_CONTEXT
-};
 
 class Token
 {
-	private :
-
-
-		std::string _info;
-		TokenType _type;
-		std::size_t _lineNumber;
-		int _serverId;
-		std::vector<Token> _path;
-		ContextType _contextType;
-
-		void _contextChecker(void);
-
 	public :
-		Token();
-		void _checkOneToken(void);
-		Token(const std::string &info, size_t lineNumber = 0, int serverId = -1);
-		Token(const Token& source);
-		Token& operator=(const Token& source);
-		~Token();
+		enum TokenType
+		{
+			UNKOWN,
+			OPENING_BRACE,
+			CLOSING_BRACE,
+			CONTEXT,
+			DIRECTIVE
+		};
 
-		static void checkAllTokensContext(std::vector<Token>& allTokens);
-		static std::vector<Token> getAllServerTokens(int serverId, std::vector<Token>& allTokens);
-		static void printAllTokensInfo(const std::vector<Token>& allTokens);
-		std::string getTokenInfo() const;
-		size_t getTokenLineNumber() const;
-		TokenType getTokenType(void) const;
-		ContextType getTokenContextType(void) const;
-		int getTokenServerId(void) const;
-		void printTokenInfo(void) const;
-		std::vector<Token> getTokenPath(void) const;
-		void setTokenPath(std::vector<Token> path);
-		void setTokenType(TokenType newType);
+		enum ContextType
+		{
+			HTTP,
+			SERVER,
+			LOCATION,
+			NOT_CONTEXT
+		};
 
-		ContextType getCurrentTokenContextType() const;
+		static void					checkAllTokensContext(std::vector<Token>& allTokens);
+		static void					printAllTokensInfo(const std::vector<Token>& allTokens);
+		static std::vector<Token>	getAllServerTokens(int serverId, std::vector<Token>& allTokens);
+		std::string					getTokenInfo() const;
+		size_t						getTokenLineNumber() const;
+		TokenType					getTokenType(void) const;
+		ContextType					getTokenContextType(void) const;
+		int							getTokenServerId(void) const;
+		void						printTokenInfo(void) const;
+		std::vector<Token>			getTokenPath(void) const;
+		void						setTokenPath(std::vector<Token> path);
+		void						setTokenType(TokenType newType);
+		ContextType					getCurrentTokenContextType() const;
+		
+									Token();
+									Token(const std::string &info, size_t lineNumber = 0, int serverId = -1);
+									Token(const Token& source);
+		Token&						operator=(const Token& source);
+									~Token();
+	private :
+		std::string			_info;
+		TokenType			_type;
+		std::size_t			_lineNumber;
+		int					_serverId;
+		std::vector<Token>	_path;
+		ContextType			_contextType;
+
+		void				_contextChecker(void);
+		void 				_checkOneToken(void);
+
+	public:
 		class InvalidTokenException : public std::exception
 		{
 			const char * what() const throw();
