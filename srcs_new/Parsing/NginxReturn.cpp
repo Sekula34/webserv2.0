39717d13b54467058075d2b0264a8988ae8196f5
Sekula34 <filip.seles@gmail.com>
2024-09-24 14:11:20 +0200

    refactor: change order of public and private and marconetting file

--- srcs_new/Parsing/NginxReturn.cpp
+++ srcs_new/Parsing/NginxReturn.cpp
@@ -2,6 +2,49 @@
 #include <iostream>
 #include <stdexcept>
 
+const bool& NginnxReturn::getFlag() const
+{
+	return _flag;
+}
+
+const int& NginnxReturn::getStatus() const 
+{
+	return (_status);
+}
+
+const std::string& NginnxReturn::getRedirectPath() const 
+{
+	return (_redirectPath);
+}
+
+void NginnxReturn::printNginxReturnInfo(void) const 
+{
+	std::cout << "Nginx Return is: " << _flag << std::endl;
+	if(_flag)
+	{
+		std::cout << "Redirect code is " << _status << std::endl;
+		std::cout << "Redirect page is " << _redirectPath << std::endl;
+	} 
+}
+
+void NginnxReturn::setFlag(bool state)
+{
+	_flag = state;
+}
+
+void NginnxReturn::setStatus(int status)
+{
+	if(status < 100 || status > 599)
+		throw std::runtime_error("Status of nginxReturn is out of range");
+	_status = status;
+}
+
+void NginnxReturn::setRedirectPath(std::string path)
+{
+	if(path.empty())
+		throw std::runtime_error("Trying to set empty redirect Path");
+	_redirectPath = path;
+}
 NginnxReturn::NginnxReturn()
 :_flag(false), _status(0), _redirectPath("DEFAULT REDIRECT Path")
 {
@@ -29,50 +72,4 @@ NginnxReturn::NginnxReturn(const NginnxReturn& source)
 NginnxReturn::~NginnxReturn()
 {
 
-}
-
-bool NginnxReturn::getFlag() const
-{
-	return _flag;
-}
-
-int NginnxReturn::getStatus() const 
-{
-	return (_status);
-}
-
-std::string NginnxReturn::getRedirectPath() const 
-{
-	return (_redirectPath);
-}
-
-void NginnxReturn::printNginxReturnInfo(void) const 
-{
-	std::cout << "Nginx Return is: " << _flag << std::endl;
-	if(_flag)
-	{
-		std::cout << "Redirect code is " << _status << std::endl;
-		std::cout << "Redirect page is " << _redirectPath << std::endl;
-	} 
-}
-
-void NginnxReturn::setFlag(bool state)
-{
-	_flag = state;
-}
-
-void NginnxReturn::setStatus(int status)
-{
-	if(status < 100 || status > 599)
-	{
-		throw std::runtime_error("Status of nginxReturn is out of range");
-	}
-	_status = status;
-}
-
-void NginnxReturn::setRedirectPath(std::string path)
-{
-	if(path.empty())
-		throw std::runtime_error("Trying to set empty redirect Path");
-	_redirectPath = path;
 }
